350fb7b62cc61208949baae5ceecf749337fb8a6
Ka1yani <kalyanijamodkar3@gmail.com>
2023-08-20 19:48:29 +0530

    ADAPT CLEAN CODE

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
-const validChoices = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors'];
 
 let getPlayerChoice = () => {
     let choice = prompt('Enter your choice').toLowerCase()
-    if ( validChoices.includes(choice) ) return choice;
+    if (VALID_CHOICES.includes(choice)) return choice;
     else {
-     alert('Choose among rock, paper or scissors');
-     return getPlayerChoice();
+        alert('Choose among rock, paper or scissors');
+        return getPlayerChoice();
     }
- } 
+}
 
 
 let getComputerChoice = () => {
-    let choiceIndex = Math.floor(Math.random() * validChoices.length);
-    return validChoices[choiceIndex];
- }
+    let choiceIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    return VALID_CHOICES[choiceIndex];
+}
+ playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    }
+    else {
+        if (VALID_CHOICES.indexOf(computerSelection) === (VALID_CHOICES.indexOf(playerSelection) + 1) % 3)
+            return `You lose: ${computerSelection} beats ${playerSelection}`;
 
-let playRound = (playerSelection, computerSelection) => {
-   if(playerSelection === computerSelection) {
-    return 'Tie';
-   }
-   else{
-    //if(validChoices.indexOf(playerSelection) > validChoices.indexOf(computerSelection)){
-    if(validChoices.indexOf(computerSelection) === (validChoices.indexOf(playerSelection)+1) % 3)
-        return `You lose: ${computerSelection} beats ${playerSelection}`;  
-
-    else
-        return `You win: ${playerSelection} beats ${computerSelection}`;
-   }
+        else
+            return `You win: ${playerSelection} beats ${computerSelection}`;
+    }
 }
 
-let game = () => {
+let playGame = () => {
     let i = 5;
-    let pc, cc, pp = 0, cp = 0;
-    while(i-->0){
-        pc = getPlayerChoice();
-        cc = getComputerChoice();
-        let roundResult = playRound(pc, cc);
+    let playerSelection, computerSelection, playerPoints = 0, computerPoints = 0;
+    while (i-- > 0) {
+        playerSelection = getPlayerChoice();
+        computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
         console.log(roundResult);
-        if(roundResult.includes('win'))
-            pp++;
-        else if(roundResult.includes('lose'))
-            cp++;
+        if (roundResult.includes('win'))
+            playerPoints++;
+        else if (roundResult.includes('lose'))
+            computerPoints++;
     }
-    if(pp > cp)
+    if (playerPoints > computerPoints)
         console.log('You won the game!');
     else
         console.log('You lost the game!');
 
-    console.log(`Score - You: ${pp}     Computer: ${cp}`);  
+    console.log(`Score - You: ${playerPoints}     Computer: ${computerPoints}`);
 }
- 
-game();
+
+playGame();
 
