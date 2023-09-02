9914669323caf19fae4ba509f3d38d00526404be
Ka1yani <kalyanijamodkar3@gmail.com>
2023-09-03 00:20:13 +0530

    ADDS UI TO THE RPS GAME

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+let playerSelection, computerSelection, playerPoints = 0, computerPoints = 0;
 
-let getPlayerChoice = () => {
-    let choice = prompt('Enter your choice').toLowerCase()
-    if (VALID_CHOICES.includes(choice)) return choice;
-    else {
-        alert('Choose among rock, paper or scissors');
-        return getPlayerChoice();
-    }
-}
-
+const buttons = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+const playerScore = document.getElementById('playerScore');
+const computerScore = document.getElementById('computerScore');
+let i = 5;
 
 let getComputerChoice = () => {
     let choiceIndex = Math.floor(Math.random() * VALID_CHOICES.length);
     return VALID_CHOICES[choiceIndex];
 }
- playRound = (playerSelection, computerSelection) => {
+playRound = (playerSelection, computerSelection) => {
     if (playerSelection === computerSelection) {
         return 'Tie';
     }
@@ -27,7 +24,47 @@ let getComputerChoice = () => {
     }
 }
 
-let playGame = () => {
+
+buttons.forEach((button) => {
+    button.addEventListener('click', function func() {
+        playerSelection = button.id;
+        computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        result.textContent = roundResult;
+        if (roundResult.includes('win'))
+            playerPoints++;
+        else if (roundResult.includes('lose'))
+            computerPoints++;
+        playerScore.textContent = playerPoints;
+        computerScore.textContent = computerPoints;
+        if (i-- === 0) {
+            if (playerPoints > computerPoints)
+                result.textContent = 'You won the game!';
+            else
+                result.textContent = 'You lost the game!';
+            i = 5;
+            reset();
+        }
+    });
+});
+
+let reset = () => {
+    playerPoints = 0;
+    computerPoints = 0;
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+};
+
+/* let getPlayerChoice = () => {
+    let choice = prompt('Enter your choice').toLowerCase()
+    if (VALID_CHOICES.includes(choice)) return choice;
+    else {
+        alert('Choose among rock, paper or scissors');
+        return getPlayerChoice();
+    }
+} */
+
+/* let playGame = () => {
     let i = 5;
     let playerSelection, computerSelection, playerPoints = 0, computerPoints = 0;
     while (i-- > 0) {
@@ -49,4 +86,4 @@ let playGame = () => {
 }
 
 playGame();
-
+ */
